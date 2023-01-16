f9175cb3c1af42fbd86cc19a95840bf65cba87f6
Roman Martyniak <110495171+Roman-Martyniak@users.noreply.github.com>
2023-01-16 13:36:34 +0200

    delete console.log

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -11,13 +11,7 @@ refs.form.addEventListener('submit', onFormSubmit);
 refs.form.addEventListener('input', throttle(onFormInput, 500));
 
 function onFormInput(event) {
-  console.log(event.target.name);
-  console.log(event.target.value);
-  // console.dir('dir', event.target);
-
   formData[event.target.name] = event.target.value;
-  // console.log('formData', formData);
-
   localStorage.setItem(LOCAL_KEY, JSON.stringify(formData));
 }
 
@@ -25,7 +19,6 @@ populateTextarea();
 
 function onFormSubmit(event) {
   event.preventDefault();
-  console.log(formData);
   localStorage.removeItem(LOCAL_KEY);
 
   event.currentTarget.reset();
@@ -35,10 +28,8 @@ function populateTextarea() {
   const savedInputTextForm = localStorage.getItem(LOCAL_KEY);
 
   if (savedInputTextForm) {
-    // console.log('populateTextarea', savedInputTextForm);
 
     formData = JSON.parse(savedInputTextForm);
-    // console.dir(' formData = ', formData);
 
     for (const key in formData) {
       refs.form.elements[key].value = formData[key];
